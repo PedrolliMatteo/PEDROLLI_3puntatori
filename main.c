593094369a082877cf73201f7cc042236cc5ef7f
PedrolliMatteo <Pedro@HP.com>
2025-10-20 16:32:47 +0200

    Finito

--- main.c
+++ main.c
@@ -1,6 +1,39 @@
+#include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
-    printf("Hello, World!\n");
+int *create_array(int n, int initial_value)
+{
+    int *array = (int *)malloc(sizeof(int) * n);
+
+    if (array == NULL)
+    {
+        printf("Errore! Array = NULL");
+        return array;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        array[i] = initial_value;
+    }
+    return array;
+}
+
+
+int main(void)
+{
+    int *ris = create_array(5, 13);
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d", ris[i]);
+        if (i < 4)
+        {
+            printf(" - ");
+        }
+    }
+    printf("\n%p", ris);
+
+    free(ris);
+    ris = NULL;
     return 0;
 }
